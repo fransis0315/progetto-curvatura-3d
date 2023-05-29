1bc36e8599c398d206af23ea3f0ca3b11a8be88f
fransis <francifede41@gmail.com>
2023-05-29 10:28:57 +0200

    add fx

--- sketch.js
+++ sketch.js
@@ -7,25 +7,46 @@
 
 **/
 
-function preload() {
-  treeModel = loadAsset("albero.glb");
-}
-
 function setup() {
   createCanvas3D(windowWidth, windowHeight);
-  
   //Change background color of the scene
-  background3D("#000000");
+  background3D("#FFFFFFFF");
   
-  hideGrid();
+  environment(SUNSET)
+  var albero = beginGroup();
+  //Change material color to orange
+  diffuse("#02AF09");
+  //Create a cylindere in 0, 0, 0 with1 radius 1;
+  sphere(0, 2.8, 0, 1);
+  diffuse("brown");
+  align(TOP);
+  cylinder(0, 0, 0, 0.3, 2);
+  diffuse("#05A50B");
+ 
+  endGroup();
   
-  var tree = clone(treeModel,0,0,0);
-
+  
   align(BOTTOM);
-  diffuse("grey");
-  box(0, 0, 0, 6, 0.5, 6);
-}
+  diffuse("#1F6E12");
+  box(0, 0, 0, 400, 0, 400);
 
+
+  var lato = 400;
+  var numberAlberi = 2000;
+  for (var i = 0; i < numberAlberi; i++) {
+    var x = random(-lato / 2, lato / 2);
+    var y = 0;
+    var z = random(-lato / 2, lato / 2);
+
+    
+    var nuovoAlbero = clone(albero, x, y, z);
+    nuovoAlbero.setScale(random(2, 3));
+  }
+  
+  pushFX(HALFTONE, 0.5)
+  pushFX(GLITCH)
+  pushFX(LUMINOSITY,)
+}
 function draw() {
   // 🔍 Never create objects here, but don't forget to have it in your code:
   // leave empty for the moment
